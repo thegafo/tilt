8757b42425882b7378a9001d56fc4a416992ea51
thegafo <gafotech1@gmail.com>
2023-03-23 19:50:23 -0400

    Initial Commit

---
+++ Tilt/TiltApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiltApp.swift
+//  Tilt
+//
+//  Created by Gabe Garcia on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TiltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
